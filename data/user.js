736e8fe03ce187a6627f8304522dacf5b18e6509
dammylarey <ovodubela@gmail.com>
2025-12-18 00:23:13 +0100

    moderater login function and added a test user

---
+++ data/user.js
@@ -0,0 +1,20 @@
+export const users  = [
+  {
+    username: "admin",
+    password: "admin"
+  },
+  {
+    username: "damilare",
+    password: "admin"
+  },
+  {
+    username: "olalekan",
+    password: "admin"
+  },
+  {
+    username: 'james',
+    password: 'admin'
+  }
+]
+
+console.log(`here are the available user ${JSON.stringify(users)}`)
--- script/script.js
+++ script/script.js
@@ -1,7 +1,70 @@
-function openLogin() {
-  document.getElementById("loginModal").style.display = "block";
+import {users} from "../data/user.js";
+
+const showLogin =  document.querySelector('.js-show')
+const hideLogin =  document.querySelector(".js-close")
+
+let isActive = false;
+
+function displayLogin(param) {
+  if (!isActive) {
+    document.querySelector('.modal').style.display = 'block'
+    isActive = param
+    console.log('login display : ' + isActive)
+  } else {
+    document.querySelector('.modal').style.display = 'none'
+    isActive = param
+    console.log('login display : ' + isActive)
+  }
 }
 
-function closeLogin() {
-  document.getElementById("loginModal").style.display = "none";
+showLogin.addEventListener('click', ()=>{
+  displayLogin(true);
+})
+// function openLogin() {
+  //   document.getElementById("loginModal").style.display = "block";
+  // }
+
+hideLogin.addEventListener('click', ()=>{
+  displayLogin(false)
+})
+// function closeLogin() {
+//   document.getElementById("loginModal").style.display = "none";
+// }
+const userName = document.querySelector('.showname')
+const intro = document.querySelector('.showintro')
+
+function login() {
+  const username = document.querySelector('.username').value.slice()
+  const password = document.querySelector('.password').value.slice()
+
+  // let html = '';
+
+  let matchedUser;
+
+  users.forEach(user => {
+    if (user.password == password && user.username == username) {
+      matchedUser = user
+      return
+    }
+    // console.log(`Username: ${user.username}, Password: ${user.password}`);
+  }); 
+
+  if (matchedUser) {
+    document.querySelector('.modal').style.display = 'none'
+    displayLogin(false)
+
+    userName.innerHTML = `Welcome, ${matchedUser.username}`
+    intro.innerHTML = 'Here you will find all your lost item'
+
+    localStorage.setItem('mainuser', JSON.stringify(matchedUser))
+  } else {
+    alert('user not found')
+  }
+
+  //const user = JSON.parse(localStorage.getItem('mainuser'))
+  // const username =  user.username || '{user.name}';
 }
+
+document.querySelector('.js-loginBtn').addEventListener('click', ()=>{
+  login()
+})
