54e2b9f1c1d3e83116bf45174baa2ef1ac08e053
DammyLarey <ovodubela@gmail.com>
2026-03-28 12:39:47 +0100

    dashboard update

---
+++ script/proof.js
@@ -0,0 +1,7 @@
+function openModal() {
+  document.getElementById("proofModal").style.display = "block";
+}
+
+function closeModal() {
+  document.getElementById("proofModal").style.display = "none";
+}
