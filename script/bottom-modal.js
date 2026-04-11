0e37753cfd80e078a3c6386dfc5a2f9fd6f5ca44
DammyLarey <ovodubela@gmail.com>
2026-04-11 17:58:30 +0100

    user page building

---
+++ script/bottom-modal.js
@@ -0,0 +1,9 @@
+    function openSheet() {
+  document.getElementById("bottomSheet").classList.add("active");
+  document.getElementById("overlay").classList.add("active");
+}
+
+function closeSheet() {
+  document.getElementById("bottomSheet").classList.remove("active");
+  document.getElementById("overlay").classList.remove("active");
+}
---
+++ script/toggle.js
@@ -0,0 +1,13 @@
+        function showTab(tabId) {
+            document.querySelectorAll(".tab").forEach(btn => {
+                btn.classList.remove("active");
+            });
+
+            document.querySelectorAll(".tab-items").forEach(tab => {
+                tab.classList.remove("active");
+            });
+
+            document.getElementById(tabId).classList.add("active");
+
+            event.target.classList.add("active")
+        }
