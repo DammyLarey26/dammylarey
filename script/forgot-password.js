dd07e6d1d91eb75824e3120c89857928b6f19f66
DammyLarey <ovodubela@gmail.com>
2026-04-26 13:39:40 +0100

    login page

--- script/forgot-password.js
+++ script/forgot-password.js
@@ -114,7 +114,7 @@ async function forgetPassword() {
     resetMsg.classList.add("success");
 
     setTimeout(() => {
-      window.location.href = "./pages/login.html";
+      window.location.href = "./login.html";
     }, 1500);
 
   } catch (err) {
